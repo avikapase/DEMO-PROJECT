7feedc8d49cb2186529a14f7de09b2429827108e
Avinash Kapase <kapase@strands.com>
2024-02-15 17:08:00 +0530

    Added testng dependecy and testng.xml to pom.xml

--- src/test/java/practice/HelloWorld.java
+++ src/test/java/practice/HelloWorld.java
@@ -1,10 +1,29 @@
 package practice;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	
+	@BeforeMethod
+	public void beforeMethod()
+	{
+		System.out.println("This is before method");
+	}
+	
+	@Test
+	public static void main() {
+		System.out.println("This is test");
 
 	}
+	
+	@AfterMethod
+	public void afterMethod()
+	{
+		System.out.println("This is before method");
+	}
+	
 
 }
